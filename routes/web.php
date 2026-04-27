dc30b7324d831ffe6519b0be6eb837b258e6aeaf
evansigma <mmarioalzaky_2428240157@mhs.mdp.ac.id>
2026-04-27 13:13:00 +0700

    laravel lagi

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\PeriodeController;
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('fakultas', FakultasController::class);
+Route::resource('/fakultas', FakultasController::class);
+
+Route::resource('/periode', PeriodeController::class);
